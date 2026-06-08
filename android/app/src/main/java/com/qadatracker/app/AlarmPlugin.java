9c2a1c9b20b395ca6db202e3a2d550b1d61df9e6
abdullahikhalilmuaz <abdullahikhalilmuaz@gmail.com>
2026-06-08 21:10:16 +0100

    'user'

--- android/app/src/main/java/com/qadatracker/app/AlarmPlugin.java
+++ android/app/src/main/java/com/qadatracker/app/AlarmPlugin.java
@@ -1,6 +1,5 @@
 package com.qadatracker.app;
 
-import com.getcapacitor.JSObject;
 import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
 import com.getcapacitor.annotation.CapacitorPlugin;
@@ -8,19 +7,12 @@ import com.getcapacitor.annotation.CapacitorPlugin;
 @CapacitorPlugin(name = "AlarmPlugin")
 public class AlarmPlugin extends Plugin {
 
-    @com.getcapacitor.annotation.PluginMethod
     public void schedule(PluginCall call) {
         int id = call.getInt("id");
         String prayer = call.getString("prayer");
         long timeMillis = call.getLong("timeMillis");
 
-        AlarmScheduler.scheduleAlarm(
-            getContext(),
-            timeMillis,
-            prayer,
-            id
-        );
-
+        AlarmScheduler.scheduleAlarm(getContext(), timeMillis, prayer, id);
         call.resolve();
     }
 }
