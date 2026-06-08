93dab6fcd4bf0b91fef0472b029fd9a2e8f8fe61
abdullahikhalilmuaz <abdullahikhalilmuaz@gmail.com>
2026-06-08 20:59:49 +0100

    use

--- android/app/src/main/java/com/qadatracker/app/AlarmPlugin.java
+++ android/app/src/main/java/com/qadatracker/app/AlarmPlugin.java
@@ -1,23 +1,18 @@
 package com.qadatracker.app;
 
+import com.getcapacitor.JSObject;
 import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
 import com.getcapacitor.annotation.CapacitorPlugin;
-import com.getcapacitor.annotation.PluginMethod;
-
-import java.util.Date;
 
 @CapacitorPlugin(name = "AlarmPlugin")
 public class AlarmPlugin extends Plugin {
 
-    @PluginMethod
+    @com.getcapacitor.annotation.PluginMethod
     public void schedule(PluginCall call) {
-
         int id = call.getInt("id");
         String prayer = call.getString("prayer");
-        String time = call.getString("time");
-
-        long timeMillis = java.time.Instant.parse(time).toEpochMilli();
+        long timeMillis = call.getLong("timeMillis");
 
         AlarmScheduler.scheduleAlarm(
             getContext(),
